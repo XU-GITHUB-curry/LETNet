5b2d214e263cea38b7a98e8a509b72f86338711d
GuoanXu <55744980+XU-GITHUB-curry@users.noreply.github.com>
2023-11-05 16:39:34 +1100

    Add files via upload

---
+++ Network/model/module/transformer.py
@@ -0,0 +1,140 @@
+# from model import common
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+# from thop import profile
+from model.patch import extract_image_patches, reduce_mean, reduce_sum, same_padding, reverse_patches
+import pdb
+import math
+
+
+def drop_path(x, drop_prob: float = 0., training: bool = False):
+    """Drop paths (Stochastic Depth) per sample (when applied in main path of residual blocks).
+    This is the same as the DropConnect impl I created for EfficientNet, etc networks, however,
+    the original name is misleading as 'Drop Connect' is a different form of dropout in a separate paper...
+    See discussion: https://github.com/tensorflow/tpu/issues/494#issuecomment-532968956 ... I've opted for
+    changing the layer and argument names to 'drop path' rather than mix DropConnect as a layer name and use
+    'survival rate' as the argument.
+    """
+    if drop_prob == 0. or not training:
+        return x
+    keep_prob = 1 - drop_prob
+    shape = (x.shape[0],) + (1,) * (x.ndim - 1)  # work with diff dim tensors, not just 2D ConvNets
+    random_tensor = keep_prob + torch.rand(shape, dtype=x.dtype, device=x.device)
+    random_tensor.floor_()  # binarize
+    output = x.div(keep_prob) * random_tensor
+    return output
+
+
+# MLP in the paper
+class Mlp(nn.Module):
+    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.ReLU, drop=0.):
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features // 4
+        self.fc1 = nn.Linear(in_features, hidden_features)
+        self.act = act_layer()
+        self.fc2 = nn.Linear(hidden_features, out_features)
+        self.drop = nn.Dropout(drop)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.drop(x)
+        x = self.fc2(x)
+        x = self.drop(x)
+        return x
+
+
+class EffAttention(nn.Module):
+    def __init__(self, dim, num_heads=8, qkv_bias=False, qk_scale=None, attn_drop=0., proj_drop=0.):
+        super().__init__()
+        self.num_heads = num_heads
+        head_dim = dim // num_heads
+        self.scale = qk_scale or head_dim ** -0.5
+
+        self.reduce = nn.Linear(dim, dim // 2,
+                                bias=qkv_bias)  # nn.Linear鍏ㄨ繛鎺ュ眰鍏惰緭鍏ヨ緭鍑轰负浜岀淮寮犻噺锛岄渶瑕佸皢4缁村紶閲忚浆鎹负浜岀淮寮犻噺涔嬪悗鎵嶈兘浣滀负杈撳叆
+        self.qkv = nn.Linear(dim // 2, dim // 2 * 3, bias=qkv_bias)
+        self.proj = nn.Linear(dim // 2, dim)
+        self.attn_drop = nn.Dropout(attn_drop)
+
+    def forward(self, x):
+        x = self.reduce(x)
+        B, N, C = x.shape
+        qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
+        q, k, v = qkv[0], qkv[1], qkv[2]
+
+        q_all = torch.split(q, math.ceil(N // 4), dim=-2)
+        k_all = torch.split(k, math.ceil(N // 4), dim=-2)
+        v_all = torch.split(v, math.ceil(N // 4), dim=-2)
+
+        output = []
+        for q, k, v in zip(q_all, k_all, v_all):
+            attn = (q @ k.transpose(-2, -1)) * self.scale  # 16*8*37*37
+            attn = attn.softmax(dim=-1)
+            attn = self.attn_drop(attn)
+            trans_x = (attn @ v).transpose(1, 2)  # .reshape(B, N, C)
+            output.append(trans_x)
+        x = torch.cat(output, dim=1)
+        x = x.reshape(B, N, C)
+        x = self.proj(x)
+        return x
+
+
+'''
+# Efficient Multi-Head Attention in the paper
+class EffAttention(nn.Module):
+    def __init__(self, dim, num_heads=8, qkv_bias=False, qk_scale=None, attn_drop=0., proj_drop=0.):
+        super().__init__()
+        self.num_heads = num_heads
+        head_dim = dim // num_heads
+        self.scale = qk_scale or 1//4##head_dim ** -0.5
+
+        self.reduce = nn.Linear(dim, dim//9, bias=qkv_bias) 
+        self.qkv = nn.Linear(dim//9, dim * 3//9, bias=qkv_bias)
+        self.proj = nn.Linear(dim//9, dim)
+        self.attn_drop = nn.Dropout(attn_drop)
+        self.proj_drop = nn.Dropout(proj_drop)
+
+    def forward(self, x):
+        x = self.reduce(x)
+        B, N, C = x.shape
+        qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
+        q, k, v = qkv[0], qkv[1], qkv[2]
+
+        attn = (q @ k.transpose(-2, -1)) * self.scale
+        attn = attn.softmax(dim=-1)
+        attn = self.attn_drop(attn)
+
+        x = (attn @ v).transpose(1, 2).reshape(B, N, C)
+        x = self.proj(x)
+        x = self.proj_drop(x)
+        return x
+'''
+
+
+## Key Module: Efficient Transformer (ET) in the paper
+class TransBlock(nn.Module):
+    def __init__(
+            self, n_feat=32, dim=288, num_heads=8, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop=0., attn_drop=0.,
+            drop_path=0., act_layer=nn.ReLU, norm_layer=nn.LayerNorm):
+        super(TransBlock, self).__init__()
+        self.dim = dim
+        self.atten = EffAttention(self.dim, num_heads=8, qkv_bias=False, qk_scale=None,
+                                  attn_drop=0., proj_drop=0.)
+        self.norm1 = nn.LayerNorm(self.dim)
+        self.mlp = Mlp(in_features=dim, hidden_features=dim // 4, act_layer=act_layer, drop=drop)
+        self.norm2 = nn.LayerNorm(self.dim)
+
+    def forward(self, x):
+        B = x.shape[0]
+        x = extract_image_patches(x, ksizes=[3, 3],
+                                  strides=[1, 1],
+                                  rates=[1, 1],
+                                  padding='same')  # 16*2304*576
+        x = x.permute(0, 2, 1)
+
+        x = x + self.atten(self.norm1(x))
+        x = x + self.mlp(self.norm2(x))
+        return x
